f5c71233d79101e7c66cb275285683ed8d1929ed
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-11 13:28:03 -0400

    Creates DropDownSelector

---
+++ src/components/form/DropDownSelector.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DropDownSelector extends Component {
+  render() {
+    return (
+      <p>This is a drop-down menu for {this.props.label}<
+    )
+  }
+}
+
+export default DropDownSelector;
