4cc75e9fc61dff017e1decb5e84d21248efe7cc7
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-11 13:29:40 -0400

    Fixes typo

--- src/components/form/DropDownSelector.js
+++ src/components/form/DropDownSelector.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class DropDownSelector extends Component {
   render() {
     return (
-      <p>This is a drop-down menu for {this.props.label}<
+      <p>This is a drop-down menu for {this.props.label}</p>
     )
   }
 }
