6111e67b9751988fb25915c208287466e0c9259b
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-11 06:58:29 -0400

    Pulling job data into state

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,36 @@ import React, { Component } from 'react';
 import Job from './Job'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      jobs: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/stubs/initial_data.json')
+      .then(this.checkResponse)
+      .then(this.getJson)
+      .then(json => this.setState({jobs: json["result"]}))
+      .catch(err => console.log('ERROR', err))
+  }
+
+  checkResponse(response) {
+    if (response.status === 200) {
+      return Promise.resolve(response)
+    } else {
+      return Promise.reject(
+        new Error(response.statusText)
+      )
+    }
+  }
+
+  getJson(response) {
+    return response.json()
+  }
+
   render() {
     return (
       <div>
