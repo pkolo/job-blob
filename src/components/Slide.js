8e15af596d7808d4228a29e4108acd6b52e14b3b
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-12 15:52:19 -0400

    Basic slide styling

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -4,10 +4,20 @@ import {StyleSheet, css} from 'aphrodite';
 
 const Slide = props => {
   return (
-    <div>
+    <div className={css(styles.slideContainer)}>
       <span>Job Blob wants to help you...</span>
     </div>
   )
 }
+
+const styles = StyleSheet.create({
+  slideContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '40px 0',
+    fontSize: '2.25em'
+  }
+
+})
 
 export default Slide;
