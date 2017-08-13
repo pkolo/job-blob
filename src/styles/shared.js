33e4343a37e155be831306641a6752e919e2f946
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-13 07:59:33 -0400

    Removes input styling

--- src/styles/shared.js
+++ src/styles/shared.js
@@ -6,23 +6,6 @@ export const button = StyleSheet.create({
   }
 })
 
-export const inputGroup = StyleSheet.create({
-  container: {
-    padding: '10px'
-  },
-  label: {
-    paddingBottom: '5px',
-    fontSize: '1.15em',
-    fontWeight: '200'
-  },
-  input: {
-    boxSizing: 'border-box',
-    width: '100%',
-    padding: '10px',
-    fontSize: '.75em'
-  }
-})
-
 export const width = StyleSheet.create({
   full: {
     width: '100%'
