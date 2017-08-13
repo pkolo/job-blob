65da7a8ff20c7f4b7da940b5c4a6d9d47d07d84c
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-13 13:23:49 -0400

    Styles error

--- src/components/ErrorMessageList.js
+++ src/components/ErrorMessageList.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { css, StyleSheet } from 'aphrodite';
+
+import { colors } from '../styles/shared'
 
 const ErrorMessageList = props => {
   return (
-    <div>
+    <div className={css(styles.error)}>
       {props.errors}
     </div>
   )
 }
 
 export default ErrorMessageList;
+
+const styles = StyleSheet.create({
+  error: {
+    color: colors.darkRed
+  }
+})
