cf488d24a1339bba709aa62da38d67c5534b7849
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-14 10:36:56 -0400

    Creates filter widget

---
+++ src/components/widget/FilterWidget.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import {StyleSheet, css} from 'aphrodite';
+import { colors, fonts } from '../../styles/shared'
+
+import DropDownSelector from '../form/DropDownSelector'
+
+class FilterWidget extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedFilter: ''
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange(e) {
+    let value = e.target.value
+    this.setState({ selectedFilter: value })
+    this.props.stateUpdater({value: value, filterType: this.props.filterType})
+  }
+
+  render() {
+    return (
+      <DropDownSelector
+        required={false}
+        label={"Filter By Category"}
+        name={"categorySelection"}
+        options={this.props.menuOptions}
+        placeholder={''}
+        selectedOption={this.state.selectedFilter}
+        changeHandler={this.handleInputChange} />
+    )
+  }
+}
+
+export default FilterWidget;
