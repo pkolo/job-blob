c3e665b04fc8e76de26c56f31fc26f92f72a9e5d
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-15 20:04:37 -0400

    Update index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configStore from './store/configStore'
-import { Provider } from 'react-redux';  
+import { Provider } from 'react-redux';
 import './index.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
