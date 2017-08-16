9a18afb8a7f000583ca5fd7b14a717beaa92c8db
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-16 07:33:14 -0400

    Adds styles and basic layout to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import {StyleSheet, css} from 'aphrodite'
+import { fonts, colors } from '../styles/shared'
+import Header from './Header'
+import Slide from './Slide'
 import JobList from './JobList'
+
 
 class App extends Component {
   render() {
     return (
-      <JobList />
+      <div className={css(styles.appContainer)}>
+        <Header />
+        <div className={css(styles.mainSection)}>
+          <Slide content={'Job Blob wants to help you...'}/>
+          <JobList />
+        </div>
+      </div>
     )
   }
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  appContainer: {
+    maxWidth: '100%',
+    fontFamily: fonts.second,
+    fontSize: '1.25em'
+  },
+  mainSection: {
+    maxWidth: '900px',
+    margin: 'auto',
+    padding: '0 10px'
+  },
+  noJobs: {
+    textAlign: 'center',
+    color: colors.darkGrey
+  }
+})
