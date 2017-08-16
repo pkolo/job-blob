571e299591bcd8f1a3c85824ba46c1010b11661d
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-15 20:03:57 -0400

    Adds store and providers to index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configStore from './store/configStore'
+import { Provider } from 'react-redux';  
 import './index.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
