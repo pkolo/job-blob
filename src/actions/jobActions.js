eec339dcaf93dc12e16e1af8a2797ad6edd4b0f8
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-16 07:09:07 -0400

    Cleans up job actions

--- src/actions/jobActions.js
+++ src/actions/jobActions.js
@@ -3,14 +3,11 @@ import JobAPI from '../api/jobApi'
 export function loadJobs() {
   return function(dispatch) {
     return JobAPI.getAllJobs()
-                 .then(jobs => {dispatch(loadJobsSuccess(jobs))})
-                 .catch(err => {throw(err)})
+      .then(response => { dispatch(loadJobsSuccess(response.result)) })
+      .catch(err => { throw(err) })
   }
 }
 
 export function loadJobsSuccess(jobs) {
-  return {
-    type: "LOAD_JOBS_SUCCESS",
-    payload: jobs
-  }
+  return {type: "LOAD_JOBS_SUCCESS", jobs}
 }
