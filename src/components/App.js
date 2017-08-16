07cc075bdf2663994f1d2c4d1d52453e1d2f89d5
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-16 08:21:29 -0400

    Pull categories from jobs

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { uniqBy } from 'lodash'
 import {StyleSheet, css} from 'aphrodite'
 import { fonts, colors } from '../styles/shared'
 import Header from './Header'
 import Slide from './Slide'
 import JobList from './JobList'
 
-
 class App extends Component {
+
+  getCategories() {
+    // A list of all categories could also be hard-coded, or served by the API
+    let categories = this.props.jobs.map(job => job.category)
+    return uniqBy(categories, 'id')
+  }
+
   render() {
     return (
       <div className={css(styles.appContainer)}>
         <Header />
         <div className={css(styles.mainSection)}>
-          <Slide content={'Job Blob wants to help you...'}/>
-          <JobList />
+          <Slide content={'Available Jobs'}/>
+          <JobList jobs={this.props.jobs} categories={this.getCategories()}/>
         </div>
       </div>
     )
--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import Job from './Job'
 
 class JobList extends Component {
   render() {
     return (
-      <h1>Hello</h1>
+      <div className={'jobList'}>
+        {this.props.jobs.map(job =>
+          <Job job={job}
+               key={job.id}
+               handleDelete={this.deleteJob}
+               menuOptions={this.props.categories}
+               stateUpdater={this.updateJob} />
+        )}
+      </div>
     )
   }
 }
