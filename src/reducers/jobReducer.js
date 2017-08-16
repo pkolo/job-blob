b2ed41c95ed1dd382e51e4fba87e9045e1a671ea
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-16 12:51:32 -0400

    Creates delete reducer

--- src/reducers/jobReducer.js
+++ src/reducers/jobReducer.js
@@ -4,6 +4,13 @@ export default function jobReducer(state = initialState.jobs, action) {
   switch(action.type) {
     case "LOAD_JOBS_SUCCESS":
       return action.jobs
+    case "DELETE_JOB_SUCCESS":
+      const newState = Object.assign([], state);
+      const indexOfJobToDelete = state.findIndex(job => {
+        return job.id == action.job.id
+      })
+      newState.splice(indexOfJobToDelete, 1)
+      return newState;
     default:
       return state;
   }
