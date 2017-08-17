74e591947f01335173bc981393651aeba6f29d34
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-17 14:03:24 -0400

    Creates job create action

--- src/actions/jobActions.js
+++ src/actions/jobActions.js
@@ -12,6 +12,23 @@ export function loadJobsSuccess(jobs) {
   return {type: "LOAD_JOBS_SUCCESS", jobs}
 }
 
+export function createJob(jobPayload) {
+  return function (dispatch) {
+    return JobAPI.createJob(jobPayload)
+      .then( (responseJob) => {
+        dispatch(createJobSuccess(responseJob))
+        return responseJob
+    }).catch(error => {
+        console.log(error)
+        throw(error)
+    })
+  }
+}
+
+export function createJobSuccess(job) {
+  return {type: "CREATE_JOB_SUCCESS", job}
+}
+
 export function deleteJob(job) {
   return function (dispatch) {
     return JobAPI.deleteJob(job)
