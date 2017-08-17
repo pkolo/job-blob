fd8e3c1469011774aee1b3708dedbf477cd5728d
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-17 14:03:46 -0400

    Adds newly created job to state

--- src/reducers/jobReducer.js
+++ src/reducers/jobReducer.js
@@ -4,10 +4,15 @@ export default function jobReducer(state = initialState.jobs, action) {
   switch(action.type) {
     case "LOAD_JOBS_SUCCESS":
       return action.jobs
+    case "CREATE_JOB_SUCCESS":
+      return [
+          ...state.filter(job => job.id !== action.job.id),
+          Object.assign({}, action.job)
+        ]
     case "DELETE_JOB_SUCCESS":
       const newState = Object.assign([], state);
       const indexOfJobToDelete = state.findIndex(job => {
-        return job.id == action.job.id
+        return job.id === action.job.id
       })
       newState.splice(indexOfJobToDelete, 1)
       return newState;
